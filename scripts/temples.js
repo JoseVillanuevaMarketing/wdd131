9223490d3efebbce2a074aad189f7f39870acb66
JoseVillanuevaMarketing <skymarkdigitalba@gmail.com>
2026-07-11 20:45:12 -0400

    w2

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,25 +1,29 @@
-// Temple Album - WDD 131
+/* ===========================
+   WDD 131 - Temple Album
+=========================== */
 
+// Select the menu button and navigation
 const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector("nav");
 
-// Toggle the navigation menu when the button is clicked
+// Toggle the navigation menu
 menuButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
 
-    // Change the menu icon
     if (navigation.classList.contains("open")) {
         menuButton.textContent = "✖";
+        menuButton.setAttribute("aria-label", "Close Navigation Menu");
     } else {
         menuButton.textContent = "☰";
+        menuButton.setAttribute("aria-label", "Open Navigation Menu");
     }
 });
 
-// Get the current date
-const today = new Date();
+// Get the current year
+const currentYear = new Date().getFullYear();
 
-// Display the current year
-document.getElementById("currentyear").textContent = today.getFullYear();
+// Display the current year in the footer
+document.getElementById("currentyear").textContent = currentYear;
 
 // Display the document's last modified date
 document.getElementById("lastModified").textContent =
